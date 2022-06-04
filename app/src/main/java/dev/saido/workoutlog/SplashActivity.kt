77a4093e88c6db0f4dfa21917913afda60487bbf
saido-noor <saidoomarnoor@gmail.com>
2022-06-04 08:21:30 +0300

    Added splash activity

--- app/src/main/java/dev/saido/workoutlog/SplashActivity.kt
+++ app/src/main/java/dev/saido/workoutlog/SplashActivity.kt
@@ -1,11 +1,14 @@
 package dev.saido.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
