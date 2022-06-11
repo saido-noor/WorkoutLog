6fd2d71e87e53cc38007e4b626d3bbed31411e0f
saido-noor <saidoomarnoor@gmail.com>
2022-06-11 09:38:57 +0300

    add bottom navigation

--- app/src/main/java/dev/saido/workoutlog/LoginActivity.kt
+++ app/src/main/java/dev/saido/workoutlog/LoginActivity.kt
@@ -27,6 +27,7 @@ class LoginActivity : AppCompatActivity() {
 
         btnLogin.setOnClickListener {
 
+
             validateLogin()
         }
         tvSingup.setOnClickListener {
@@ -40,13 +41,20 @@ class LoginActivity : AppCompatActivity() {
     fun validateLogin(){
         val email = etEmail.text.toString()
         val password = etPassword.text.toString()
+        var error = false
 
         if (email.isBlank()) {
             tilEmail.error=getString(R.string.email_required)
+            error=true
 
         }
         if(password.isBlank()){
             tilPassword.error=getString(R.string.password_required)
+            error=true
+        }
+        if(!error){
+            startActivity(Intent(this,singupActivity::class.java))
+            finish()
         }
     }
 
