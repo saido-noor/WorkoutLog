4c2cb2ebacc283074ed663c766b177b5c999138e
saido-noor <saidoomarnoor@gmail.com>
2022-06-18 09:00:54 +0300

    added a supportfragmentmanager for displaying everyfragment

--- app/src/main/java/dev/saido/workoutlog/homeActivity.kt
+++ app/src/main/java/dev/saido/workoutlog/homeActivity.kt
@@ -2,10 +2,50 @@ package dev.saido.workoutlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class homeActivity : AppCompatActivity() {
+    lateinit var fcvHome: FragmentContainerView
+    lateinit var bnvHome: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        castView()
+        setupBottomNav()
+    }
+
+    fun castView() {
+        fcvHome = findViewById(R.id.fcvHome)
+        bnvHome = findViewById(R.id.bnvHome)
+    }
+
+    //switch between the fragments
+    fun setupBottomNav() {
+        bnvHome.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.plan -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.fcvHome, PlanFragment())
+                    transaction.commit()
+                    true
+                }
+
+                R.id.track -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.fcvHome, TrackFragment())
+                    transaction.commit()
+                    true
+
+                }
+                R.id.profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fcvHome, ProfileFragment()).commit()
+                    true
+
+                }
+                else -> false
+            }
+        }
     }
 }
